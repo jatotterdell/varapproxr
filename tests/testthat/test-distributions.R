9f8ebcf2f42fbaacb1fc662a9e6e40a2f74fe55d
jatotterdell <jatotterdell@gmail.com>
2023-07-13 13:38:58 +1000

    Format documents

--- tests/testthat/test-distributions.R
+++ tests/testthat/test-distributions.R
@@ -1,41 +1,38 @@
 test_that("Multivariate normal functions work as expected", {
-  
   expect_error(mvn_entropy(1))
-  expect_error(mvn_entropy(matrix(c(1,2,3,4), 2, 2)))
+  expect_error(mvn_entropy(matrix(c(1, 2, 3, 4), 2, 2)))
   expect_error(mvn_entropy(rbind(diag(1, 2), 0)))
   expect_equal(mvn_entropy(diag(1, 1)), 0.5 * (1 + log(2) + log(pi)))
   expect_equal(mvn_entropy(diag(2, 1)), 1.765512, tolerance = 1e-5)
-  expect_equal(mvn_entropy(matrix(c(2, 1, 1, 2), 2, 2)), 0.5*(2*(1 + log(2) + log(pi)) + log(det(matrix(c(2, 1, 1, 2), 2, 2)))))
-  
+  expect_equal(
+    mvn_entropy(matrix(c(2, 1, 1, 2), 2, 2)),
+    0.5 * (2 * (1 + log(2) + log(pi)) + log(det(matrix(c(2, 1, 1, 2), 2, 2))))
+  )
+
   expect_error(mvn_E_lpdf(1, diag(1), 1, 1))
   expect_error(mvn_E_lpdf(1, 1, 1, diag(1)))
-  
 })
 
 
 test_that("Inverse Gamma functions work as expected", {
-  
   expect_true(is.na(ig_E(2, -2)))
   expect_true(is.na(ig_E(1, 2)))
   expect_equal(ig_E(2, 2), 2)
-  
+
   expect_equal(ig_E_inv(1, 2), 0.5)
-  
+
   expect_equal(ig_E_log(1, 2), log(2) - digamma(1))
-  
 })
 
 
 test_that("Inverse Wishart functions work as expected", {
-  
-  expect_error(inv_wishart_E_invX(2, diag(1,2)))
-  expect_error(inv_wishart_E_invX(3, matrix(c(1,2,3,4), 2, 2)))
+  expect_error(inv_wishart_E_invX(2, diag(1, 2)))
+  expect_error(inv_wishart_E_invX(3, matrix(c(1, 2, 3, 4), 2, 2)))
   expect_equal(inv_wishart_E_invX(4, diag(1, 2)), diag(4, 2))
-  
-  expect_error(inv_wishart_E_logdet(2, diag(1,2)))
-  expect_error(inv_wishart_E_logdet(3, matrix(c(1,2,3,4), 2, 2)))
+
+  expect_error(inv_wishart_E_logdet(2, diag(1, 2)))
+  expect_error(inv_wishart_E_logdet(3, matrix(c(1, 2, 3, 4), 2, 2)))
   expect_equal(inv_wishart_E_logdet(2, diag(1, 1)), log(0.5) + digamma(2))
-  expect_equal(inv_wishart_E_logdet(3, diag(1, 2)), 2*log(0.5) + digamma(3) + digamma(2.5))
-  expect_equal(inv_wishart_E_logdet(4, diag(2, 2)), 2*log(0.5) + digamma(4) + digamma(3.5) + log(det(diag(2, 2))))
-  
+  expect_equal(inv_wishart_E_logdet(3, diag(1, 2)), 2 * log(0.5) + digamma(3) + digamma(2.5))
+  expect_equal(inv_wishart_E_logdet(4, diag(2, 2)), 2 * log(0.5) + digamma(4) + digamma(3.5) + log(det(diag(2, 2))))
 })
--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -1,26 +1,26 @@
 # Test helper functions
 
 test_that("Special mathematics functions work as expected", {
-  
   expect_error(lmvgamma(-1, 2))
   expect_error(lmvgamma(2, 0))
   expect_equal(lmvgamma(2, 1), 0)
   expect_equal(lmvgamma(3, 1), 0.693147, tolerance = 1e-5)
   expect_equal(lmvgamma(3, 2), 1.550195, tolerance = 1e-5)
   expect_equal(lmvgamma(4, 3), 5.402975, tolerance = 1e-5)
-  
+
   expect_error(mvdigamma(-1, 2))
   expect_error(mvdigamma(2, 0))
   expect_equal(mvdigamma(2, 1), digamma(2))
   expect_equal(mvdigamma(2, 2), digamma(2) + digamma(1.5))
   expect_equal(mvdigamma(4, 3), 3.282059, tolerance = 1e-5)
-  
 })
 
 
 test_that("Matrix operators work as expected", {
-  M1 <- matrix(c(1,2,3,4), 2, 2)
+  M1 <- matrix(c(1, 2, 3, 4), 2, 2)
   M2 <- matrix(c(1, 1), 1, 2)
-  expect_equal(blockDiag(list(M1, M2)),
-               matrix(c(1, 2, 0, 3, 4, 0, 0, 0, 1, 0, 0, 1), 3, 4))
+  expect_equal(
+    blockDiag(list(M1, M2)),
+    matrix(c(1, 2, 0, 3, 4, 0, 0, 0, 1, 0, 0, 1), 3, 4)
+  )
 })
